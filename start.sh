c48bf28e368fe00c2427027b21e932d7a6a977c4
zofvbeaf <zofvbeaf@gmail.com>
2019-06-27 15:15:19 +0800

    update new md

---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# if can't be accessed, do
+# iptables -A OUTPUT -p tcp --sport 15722 -j ACCEPT
+# iptables -A INPUT -p tcp --dport 15722 -j ACCEPT 
+hexo server -i 192.168.1.52 -p 15722 --silent g -d
